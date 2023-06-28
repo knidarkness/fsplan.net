e581f0f605cd9d09cd2ee97e5d2a3135e8fd47da
Sergey Dubovyk <knidarkness@gmail.com>
2023-06-28 22:06:26 +0200

    Update node version and add yarn install in build script
    Node version in package.json has been specifically set to 16.x to avoid potential compatibility issues with higher versions. Also, "yarn install --production=false" was added to the build-do.sh script to ensure all dependencies, including devDependencies, are installed before running tests and building.

--- build-do.sh
+++ build-do.sh
@@ -1,5 +1,6 @@
 mkdir temp
 curl  -o "./temp/airport-data-with-nearby-by-region.json" https://find-sim-routes.fra1.digitaloceanspaces.com/airports-data/airport-data-with-nearby-by-region.json
 mv ./temp/airport-data-with-nearby-by-region.json ./static/airport-data-with-nearby-by-region.json
+yarn install --production=false
 yarn test
 yarn build
